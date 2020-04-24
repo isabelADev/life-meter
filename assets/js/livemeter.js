fff8ee6f14f91ccadf365957276e542ae8ba299b
Isabel Almellones <isabel.almellones@gmail.com>
2020-04-24 12:47:13 +0200

    Added hp time consumption

--- assets/js/livemeter.js
+++ assets/js/livemeter.js
@@ -8,19 +8,20 @@ class LiveMeter {
     mp = this.maxMp;
 
     constructor(maxHp, maxMP, startHp, startMP, hpTitle, mpTitle) {
-        this.hpTitle = hpTitle;
-        this.mpTitle = mpTitle;
-        this.maxHP = maxHp;
-        this.maxMp = maxMP;
-        this.hp = startHp;
-        this.mp = startMP;
+            this.hpTitle = hpTitle;
+            this.mpTitle = mpTitle;
+            this.maxHP = maxHp;
+            this.maxMp = maxMP;
+            this.hp = startHp;
+            this.mp = startMP;
     }
 
     addHP(amount) {
         var auxAmount = amount;
         if (typeof amount === 'number') {
             auxAmount = parseInt(amount);
-            this.hp = Math.min(this.maxHP, this.hp + auxAmount);
+            let finalResult = Math.min(this.maxHP, this.hp + auxAmount);
+            this.hp = Math.max(0, finalResult);
         } else {
             this.hp = Math.max(0, this.hp - auxAmount);
         }
@@ -30,11 +31,10 @@ class LiveMeter {
         var auxAmount = amount;
         if (typeof amount === 'number') {
             auxAmount = parseInt(amount);
-            if (auxAmount > 0) {
-                this.mp = Math.min(this.maxMP, this.mp + auxAmount);
-            } else {
-                this.mp = Math.max(0, this.mp - auxAmount);
-            }
+            let finalResult = Math.min(this.maxMP, this.mp + auxAmount);
+            this.mp = Math.max(0, finalResult);
+        } else {
+            this.mp = Math.max(0, this.mp - auxAmount);
         }
     }
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
+'strict mode';
 var liveMeter;
+var streamTime = {
+    hours: 0,
+    mins: 5,
+};
 
 window.onload = function () {
 
-    let socket = new WebSocket("ws://ip:port");
+    var socket;
+
+    socket = new WebSocket("ws://localhost:8081");
     socket.onopen = function (event) {
         console.log("Connection established with Twitch bot");
     };
+    socket.onclose = function (event) {
+        console.log(event);
+    };
 
-    liveMeter = new LiveMeter(100, 100, 60, 80, 'HP', 'MP');
+    liveMeter = new LiveMeter(100, 100, 100, 80, 'HP', 'MP');
     let healthBar = document.getElementById("health"),
+        healthBGBar = document.getElementById("hpBGBar"),
+        magicBGBar = document.getElementById("mpBGBar"),
         magicBar = document.getElementById("magic"),
         hpNumber = document.getElementById("hpNumber"),
         mpNumber = document.getElementById("mpNumber");
 
-    onPropModified('hp', hpNumber, healthBar);
-    onPropModified('mp', mpNumber, magicBar);
+    onPropModified('hp', hpNumber, healthBar, healthBGBar);
+    onPropModified('mp', mpNumber, magicBar, magicBGBar);
 
     liveMeter = new Proxy(liveMeter, {
         set: (target, name, value) => {
@@ -26,17 +38,26 @@ window.onload = function () {
                 case 'maxHP':
                 case 'hp':
                     console.log("proxy " + liveMeter.hp);
-                    return onPropModified('hp', hpNumber, healthBar);
+                    return onPropModified('hp', hpNumber, healthBar, healthBGBar);
                     break;
                 case 'maxMP':
                 case 'mp':
-                    return onPropModified('mp', mpNumber, magicBar);
+                    return onPropModified('mp', mpNumber, magicBar, magicBGBar);
                     break;
                 default:
             }
             return false;
         }
     });
+
+    var elapsedTime = 0;
+    var totalStreamTime = getStreamTime();
+    setInterval(function () {
+        elapsedTime += 1;
+        if (liveMeter.hp > 0) {
+            liveMeter.addHP(-(1/totalStreamTime*100));
+        }
+    }, 60000);
 
     socket.onmessage = function (event) {
         let data = event.data;
@@ -66,18 +87,48 @@ window.onload = function () {
         console.log("Message received: " + event.data);
     };
 
-    function onPropModified(name, numberElem, barElem) {
+    function getStreamTime() {
+        var time = 0;
+        if (streamTime.hours) {
+            time += streamTime.hours * 60;
+        }
+        if (streamTime.mins) {
+            time += streamTime.mins;
+        }
+        return time;
+    }
+
+    function addWarningClasses(value, numberElem, barElem) {
+            let warning = 'warning';
+
+            if (value > 20) {
+                if (numberElem.classList.contains(warning)) {
+                    numberElem.classList.remove(warning);
+                    barElem.classList.remove(warning);
+                }
+            } else {
+                if (!numberElem.classList.contains(warning)) {
+                    numberElem.classList.add(warning);
+                    barElem.classList.add(warning);
+                }
+            }
+    }
+
+    function onPropModified(name, numberElem, barElem, bgBarElem) {
         try {
-            let value = name in liveMeter && liveMeter[name] ? liveMeter[name].toString() : '100';
+            let value = name in liveMeter && typeof liveMeter[name] === 'number' ? liveMeter[name].toString() : '100';
             while (value.length < 3) {
                 value = '0' + value;
+            }
+            if (name === 'hp') {
+                addWarningClasses(value, numberElem, bgBarElem);
             }
             numberElem.innerHTML = value;
             barElem.style.width = (liveMeter.hp / liveMeter.maxHP * 100) + "%";
         } catch (err) {
             return false;
         }
-        return true
+        return true;
     }
 
 };
